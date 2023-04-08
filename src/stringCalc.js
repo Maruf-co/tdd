4f6ee160e79df3e4bb298a53e7fccd32859a44ac
Maruf Asatullaev <a-maruf@yandex-team.ru>
2023-04-08 11:50:31 +0300

    wave 6.1

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,7 +1,19 @@
 const splitter = (str) => {
-    return str.replace(/\D/g, " ").trim().split(' ');
+    const arr = str.replace(/\D/g, " ").trim().split(' ');
+    let ans = false
+    arr.forEach(el => {
+        if(el === '') {
+            throw 'negatives not allowed'
+        }
+    })
+    return arr.reduce((acc, el) => acc + parseInt(el), 0)
 }
 
-const calc = (str) => str !== '' ? splitter(str).reduce((acc, cur) => acc + parseInt(cur), 0) : 0;
+
+
+const calc = (str) =>  str !== '' ? splitter(str) : 0;
+
+// const str = ''
+// console.log(calc(str))
 
 export default calc;
