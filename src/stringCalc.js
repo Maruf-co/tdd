49662743a8182cb44940793f1fc795ba6569ee46
Maruf Asatullaev <a-maruf@yandex-team.ru>
2023-04-08 11:38:27 +0300

    5.1 wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,3 +1,7 @@
-const calc = (str) => str !== '' ? str.split(',').reduce((acc, cur) => acc + parseInt(cur), 0) : 0;
+const splitter = (str) => {
+    return str.replace(/\D/g, " ").trim().split(' ');
+}
+
+const calc = (str) => str !== '' ? splitter(str).reduce((acc, cur) => acc + parseInt(cur), 0) : 0;
 
 export default calc;
